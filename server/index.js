d6a5cf062d5a387eda2cba599a7d5c690d0b25ff
Gabriel <gabriel.grover@aofl.com>
2017-09-18 13:26:09 -0700

    added auth routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { googleClientID, googleClientSecret } = require('./config/keys');
+
+passport.use(
+	new GoogleStrategy({
+		clientID: googleClientID,
+		clientSecret: googleClientSecret,
+		callbackURL: "/auth/google/callback"
+	}, (accessToken, refreshToken, profile, done) => {
+		
+	})
+);
+
+app.get("/auth/google", passport.authenticate("google", {
+		scope: ["profile", "email"]
+	})
+);
+
+app.get("/auth/google/callback", passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => res.send({hi: "there"}));
-
-
 app.listen(PORT);
