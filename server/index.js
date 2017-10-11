7fe7f036918ac60f8d2779dd945a0df445b3fec4
Gabriel <gabriel.grover@aofl.com>
2017-10-11 07:33:48 -0700

    authentication finished

--- server/index.js
+++ server/index.js
@@ -1,25 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { mongoURI } = require('./config/keys');
+require('./models/User')
+require('./services/passport');
+
+mongoose.connect(mongoURI);
+
 const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const { googleClientID, googleClientSecret } = require('./config/keys');
-
-passport.use(
-	new GoogleStrategy({
-		clientID: googleClientID,
-		clientSecret: googleClientSecret,
-		callbackURL: "/auth/google/callback"
-	}, (accessToken, refreshToken, profile, done) => {
-		
-	})
-);
-
-app.get("/auth/google", passport.authenticate("google", {
-		scope: ["profile", "email"]
-	})
-);
-
-app.get("/auth/google/callback", passport.authenticate('google'));
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
